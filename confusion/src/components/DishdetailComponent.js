d1080776b9f69739497afef5a9d75ec7590f0381
Samuel Levin <levinsamuel000@gmail.com>
2018-11-13 10:47:23 -0800

    change to functional components since they have no logic

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -2,66 +2,64 @@ import React, {Component} from 'react';
 import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
 
 
-class DishDetail extends Component {
+const DishDetail = props => {
+  if (props.dish) {
 
-  render() {
-    if (this.props.dish) {
-
-      return (
-        <div className="row">
-          <div className="col-12 col-md-5 m-1">
-            {this.renderDish(this.props.dish)}
-          </div>
-          <div className="col-12 col-md-5 m-1">
-            {this.renderComments(this.props.dish.comments)}
-          </div>
-        </div>
-      );
-    } else {
-      return (<div></div>);
-    }
-  }
-
-  renderDish(dish) {
     return (
-      <div>
-        <Card>
-          <CardImg width="100%" src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle heading="true">{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
+      <div className="row">
+        <div className="col-12 col-md-5 m-1">
+          <RenderDish dish={props.dish}/>
+        </div>
+        <div className="col-12 col-md-5 m-1">
+          <RenderComments comments={props.dish.comments}/>
+        </div>
+      </div>
+    );
+  } else {
+    return (<div></div>);
+  }
+}
+
+function RenderDish({dish}) {
+  return (
+    <div>
+      <Card>
+        <CardImg width="100%" src={dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle heading="true">{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  )
+}
+
+function RenderComments({comments}) {
+
+  if (comments) {
+
+    const commsRender = comments.map((comm) => {
+        console.debug("date value: ", comm.date)
+        const dt = new Date(comm.date);
+        return (
+            <div key={comm.id} className="mt-2">
+              <li className="">{comm.comment}</li>
+              <li className="mt-1">-- {comm.author}, {dt.toLocaleDateString()}</li>
+            </div>
+        );
+    });
+    return (
+      <div className="text-left">
+        <h4>Comments</h4>
+        <ul className="list-unstyled">
+          {commsRender}
+        </ul>
       </div>
     )
+  } else {
+    return (<div></div>)
   }
+}
 
-  renderComments(comments) {
-
-    if (comments) {
-
-      const commsRender = comments.map((comm) => {
-          console.debug("date value: ", comm.date)
-          const dt = new Date(comm.date);
-          return (
-              <div key={comm.id} className="mt-2">
-                <li className="">{comm.comment}</li>
-                <li className="mt-1">-- {comm.author}, {dt.toLocaleDateString()}</li>
-              </div>
-          );
-      });
-      return (
-        <div className="text-left">
-          <h4>Comments</h4>
-          <ul className="list-unstyled">
-            {commsRender}
-          </ul>
-        </div>
-      )
-    } else {
-      return (<div></div>)
-    }
-  }
-};
 
 export default DishDetail;
