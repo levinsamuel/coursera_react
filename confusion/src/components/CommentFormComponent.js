e309fd615aea76c1f6c41a4e5976e010e529d605
Samuel Levin <levinsamuel000@gmail.com>
2018-11-20 20:50:51 -0800

    change form to match expected values

--- confusion/src/components/CommentFormComponent.js
+++ confusion/src/components/CommentFormComponent.js
@@ -4,7 +4,7 @@ import {Button, Modal, ModalBody, ModalHeader, Label, Col, Row}
   from 'reactstrap';
 import {Control, LocalForm} from 'react-redux-form';
 import {ValidatedInput} from './templates/FormComponent';
-import {REQUIRED, MINLENGTH, MAXLENGTH, ISNUMBER, VALIDEMAIL}
+import {REQUIRED, MINLENGTH, MAXLENGTH}
   from '../shared/rules';
 
 class CommentForm extends Component {
@@ -31,38 +31,27 @@ class CommentForm extends Component {
           <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
           <ModalBody>
             <LocalForm onSubmit={values => this.handleSubmit(values)}>
-              <ValidatedInput field="firstname" name="First Name" type="text"
-                  vrules={{REQUIRED, maxLength: MAXLENGTH(15), minLength: MINLENGTH(3)}} />
-              <ValidatedInput field="lastname" name="Last Name" type="text"
-                  vrules={{REQUIRED, maxLength: MAXLENGTH(15), minLength: MINLENGTH(3)}} />
-              <ValidatedInput field="telnum" name="Tel. Number" type="tel" vrules={{REQUIRED, ISNUMBER}} />
-              <ValidatedInput field="email" name="Email" type="email" vrules={{REQUIRED, VALIDEMAIL}} />
-
               <Row className="form-group">
-                <Col md={{size: 6, offset: 2}}>
-                  <div className="form-check">
-                    <Label check>
-                      <Control.checkbox type="checkbox" id="agree" name="agree"
-                          placeholder="Email" model=".agree"
-                          className="form-check-input"/>
-                        {' '}<strong>May we contact you?</strong>
-                    </Label>
-                  </div>
-                </Col>
-                <Col md={{size: 3, offset: 1}}>
-                  <Control.select type="select" id="contactType" name="contactType"
-                      placeholder="Email" model=".contactType"
+                <Label htmlFor="rating" md={2}>Rating</Label>
+                <Col md={10}>
+                  <Control.select type="select" id="rating" name="rating"
+                      model=".rating"
                       className="form-control">
-                    <option>Tel.</option>
-                    <option>Email</option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
                   </Control.select>
                 </Col>
               </Row>
+              <ValidatedInput field="author" name="Author" type="text"
+                  vrules={{REQUIRED, maxLength: MAXLENGTH(15), minLength: MINLENGTH(3)}} />
               <Row className="form-group">
                 <Label htmlFor="message" md={2}>Your Feedback</Label>
                 <Col md={10}>
-                  <Control.textarea type="textarea" id="message" name="message"
-                      rows="12" model=".message"
+                  <Control.textarea type="textarea" id="comment" name="comment"
+                      rows="12" model=".comment"
                       className="form-control"/>
                 </Col>
               </Row>
