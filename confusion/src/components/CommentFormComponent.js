36c1db7422a5c198eb2643e846d06431f46ea0fb
Samuel Levin <levinsamuel000@gmail.com>
2018-11-20 19:30:04 -0800

    add sample modal

--- confusion/src/components/CommentFormComponent.js
+++ confusion/src/components/CommentFormComponent.js
@@ -1,20 +1,58 @@
 
 import React, {Component} from 'react';
-import {Button} from 'reactstrap';
+import {Button, Modal, ModalBody, ModalHeader, Form, FormGroup, Label, Input}
+  from 'reactstrap';
 
 class CommentForm extends Component {
 
   constructor(props) {
     super(props);
 
+    this.state = {
+      isModalOpen: false
+    };
+
+    this.toggleModal = this.toggleModal.bind(this);
   }
 
   render() {
     return (
-      <Button type="button" color="light" onClick={() => console.log('comment')}>
-        <span className="fa fa-pencil fa-lg"></span> Submit Comment
-      </Button>
+      <>
+        <Button type="button" color="light" onClick={this.toggleModal}>
+          <span className="fa fa-pencil fa-lg"></span> Submit Comment
+        </Button>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.handleLogin}>
+              <FormGroup>
+                <Label htmlFor="username">Username</Label>
+                <Input type="text" id="username" name="username"
+                    innerRef={(input) => this.username = input}/>
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="password">Password</Label>
+                <Input type="password" id="password" name="password"
+                    innerRef={(input) => this.password = input}/>
+              </FormGroup>
+              <FormGroup check>
+                <Label check>
+                  <Input type="checkbox" name="remember"
+                      innerRef={(input) => this.remember = input}/>
+                Remember Me</Label>
+              </FormGroup>
+              <Button type="submit" value="submit" color="primary">Login</Button>
+            </Form>
+          </ModalBody>
+        </Modal>
+      </>
     );
+  }
+
+  toggleModal() {
+    this.setState((state, props) => ({
+        isModalOpen: !state.isModalOpen
+    }));
   }
 }
 
--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
-import DishDetail from './DishdetailComponent';
 import {Link} from 'react-router-dom';
 
 const Menu = props => {
