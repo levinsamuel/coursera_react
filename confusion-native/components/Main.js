070a893e1e80fadaad890f7641bd65b30e9ae055
Samuel Levin <levinsamuel000@gmail.com>
2019-01-01 10:24:39 -0800

    add dish detail component, onPress for pressing menu item and displaying dish

--- confusion-native/components/Main.js
+++ confusion-native/components/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Menu from './Menu';
+import DishDetail from './Dishdetail'
 import {DISHES} from '../shared/dishes';
 
 class Main extends React.Component {
@@ -8,13 +9,26 @@ class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      dishes: DISHES
-    }
+      dishes: DISHES,
+      selectedDish: null
+    };
   }
 
+  onDishSelect(dishId) {
+    console.log('pressed with dish id: ', dishId)
+    this.setState({selectedDish: dishId});
+  }
   render() {
     return (
-      <Menu dishes={this.state.dishes}/>
+      <View style={{flex:1}} >
+        <Menu dishes={this.state.dishes}
+            onPress={dishId => this.onDishSelect(dishId)}
+          />
+        <DishDetail dish={this.state.dishes.filter(
+              dish => dish.id === this.state.selectedDish
+            )[0]}
+          />
+      </View>
     )
   }
 }
